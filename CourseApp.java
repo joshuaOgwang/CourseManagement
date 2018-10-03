f59a39d65312e5264017578b6b6ec943d021ccb8
OGWANG JOSHUA JAMES <joshuaogwang46@gmail.com>
2018-10-03 08:56:58 +0300

    added the getter and setter methods for the class Person data members

--- CourseApp.java
+++ CourseApp.java
@@ -1,4 +1,8 @@
 /*
+*imports
+*/
+import java.util.Date;
+/*
 *super class Person
 */
 class Person {
@@ -7,7 +11,7 @@ class Person {
   */
   String name;
   String email;
-  Date birthDate
+  Date birthDate;
 
   /*
   *person constructor with a @param name
@@ -19,9 +23,45 @@ class Person {
   /*
   *person constructor with @params name, email and birthDate
   */
-  public Person(String name, String email Date birthDate) {
+  public Person(String name, String email, Date birthDate) {
     this.name = name;
     this.email = email;
     this.birthDate = birthDate;
+  }
+
+  /*
+  * getters and setters for class Person instance variables
+  */
+  public String getName() {
+    return this.name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public Date getBirthDate() {
+    return this.birthDate;
+  }
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
+
+  public void setEmail(String newEmail) {
+    this.email = newEmail;
+  }
+
+  public void setBirthDate(Date newBirthDate) {
+    this.birthDate = newBirthDate;
+  }
+}
+
+/*
+*main class CourseApp
+*/
+public class CourseApp{
+  public static void main(String[] args) {
+
   }
 }
